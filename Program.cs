4f25b318f799c9d8a480d4570085f34c8d5377c7
Wongel Lakew <wongellakewteshome@gmail.com>
2026-05-26 18:28:03 +0300

    feat(module-1/session-1): reproduce legacy null reference bug

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Execute Exercise 1
+RunExercise1();
+
+void RunExercise1()
+{
+    Console.WriteLine("--- Exercise 1: Null Safety ---");
+
+    // --- Step 1: Reproduce Legacy Bug ---
+    
+    string region = null; 
+    // Console.WriteLine(region.ToUpper()); // Uncommenting this would cause a NullReferenceException
+
+   
+}
